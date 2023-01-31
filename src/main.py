ba4de7d8b185e436970b9a4a47c1affc08abea23
payam <payam.code@gmail.com>
2023-01-31 22:50:42 +0000

    making things work

--- src/main.py
+++ src/main.py
@@ -4,8 +4,9 @@ import uvicorn
 
 from fastapi import FastAPI, Request, Form
 # from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseSettings, BaseModel
 
-from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
@@ -19,34 +20,83 @@ openai.api_key = settings.OPENAI_API_KEY
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "https://genoai.com/",
+     "http://localhost:3000",
+     "http://127.0.0.1:3000",
+    "http://localhost:3001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def index():
     return { "message": "hello world???"}
 
+class Item(BaseModel):
+    animal: str
 
 @app.post("/")
-async def index(request: Request, animal: str= Form(...)):
+async def index(item: Item):
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=generate_prompt(animal),
-        temperature=0.6,
+        prompt=item.animal,
+        max_tokens=800,
+        temperature=0.4,
     )
     result = response.choices[0].text
     print("result", result)
     return {"result": result}
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+# myDict = {
+#       "id": "st-1",
+#       "title": "place a title based on the context of story here",
+#       "slides": [
+#           {
+#               "title": "Give a title to this slide",
+#               "content": "This is the first slide of Story 1",
+#               "imagePrompt": "use your imagination to write a prompt for the AI to generate an image based on the content of this slide, be highly detaile and also abstract but contextual"
+#           },
+#           {
+#               "title": "Give a title to this slide",
+#               "content": "This is the second slide of Story 1",
+#               "imagePrompt": "use your imagination to write a prompt for the AI to generate an image based on the content of this slide, be highly detaile and also abstract but contextual"
+#           },
+#           {
+#               "title": "Give a title to this slide",
+#               "content": "This is the third slide of Story 1",
+#               "imagePrompt": "use your imagination to write a prompt for the AI to generate an image based on the content of this slide, be highly detaile and also abstract but contextual"
+#           }
+#       ]
+#     },
+
+# def generate_prompt(animal):
+#     if animal is None:
+#         return "Error: animal is undefined"
+
+#     return '''
+#     Input: {}
+#     ----------------------------
+#     Write a detailed story for the product, idea or write a novel.
+#     detect the language of user and translate your story to that language.
+#     here is just an example of how the JSON output should look like. (Important: do not use double quites inside the texts):
+
+#     {}
+
+#     -----------------------------
+#     Here are the results:
+#     '''.format(
+#         animal.capitalize(),
+#         myDict
+#     )
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
